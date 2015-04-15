a94cc045e26d56523c9edc0d6ac4b01c62b5e66c
LyneLiu <595596226@qq.com>
2015-04-15 23:11:45 +0800

    Erlang source code

---
+++ otp_src_17.4/erts/emulator/i686-pc-linux-gnu/erl_version.h
@@ -0,0 +1,6 @@
+/* This file was created by 'make_version' -- don't modify. */
+#define ERLANG_OTP_RELEASE "17"
+#define ERLANG_OTP_VERSION "17.4"
+#define ERLANG_VERSION "6.3"
+#define ERLANG_COMPILE_DATE "Sun Jan 11 03:12:15 2015"
+#define ERLANG_ARCHITECTURE "i686-pc-linux-gnu"
